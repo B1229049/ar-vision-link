e7da483d4a3f68c8dc0ac1050e53d1d67fd69042
prince <a0909070650@gmail.com>
2026-08-13 17:50:31 +0800

    modify商城

--- src/data/storeCatalogData.js
+++ src/data/storeCatalogData.js
@@ -3,27 +3,28 @@
 export const STORE_CATALOG_SOURCE = [
   {
     "id": "outfit-01",
-    "name": "套裝 01",
+    "name": "牛仔很忙",
+    "badge": "PREVIEW",
     "published": true,
     "pieces": {
       "hair": {
         "id": "store-outfit-01-hair",
-        "label": "套裝 01・頭部",
+        "label": "牛仔很忙・頭部",
         "front": "store/hair/頭髮01.png"
       },
       "face": {
         "id": "store-outfit-01-face",
-        "label": "套裝 01・臉部",
+        "label": "牛仔很忙・臉部",
         "front": "store/face/表情01.png"
       },
       "top": {
         "id": "store-outfit-01-top",
-        "label": "套裝 01・上半身",
+        "label": "牛仔很忙・上半身",
         "front": "store/top/上衣01.png"
       },
       "bottoms": {
         "id": "store-outfit-01-bottoms",
-        "label": "套裝 01・下半身",
+        "label": "牛仔很忙・下半身",
         "front": "store/bottoms/褲裝01.png"
       }
     },
@@ -52,27 +53,28 @@ export const STORE_CATALOG_SOURCE = [
   },
   {
     "id": "outfit-02",
-    "name": "套裝 02",
+    "name": "傑克伯爵",
+    "badge": "PREVIEW",
     "published": true,
     "pieces": {
       "hair": {
         "id": "store-outfit-02-hair",
-        "label": "套裝 02・頭部",
+        "label": "傑克伯爵・頭部",
         "front": "store/hair/頭髮02.png"
       },
       "face": {
         "id": "store-outfit-02-face",
-        "label": "套裝 02・臉部",
+        "label": "傑克伯爵・臉部",
         "front": "store/face/表情02.png"
       },
       "top": {
         "id": "store-outfit-02-top",
-        "label": "套裝 02・上半身",
+        "label": "傑克伯爵・上半身",
         "front": "store/top/上衣02.png"
       },
       "bottoms": {
         "id": "store-outfit-02-bottoms",
-        "label": "套裝 02・下半身",
+        "label": "傑克伯爵・下半身",
         "front": "store/bottoms/褲裝02.png"
       }
     },
@@ -101,28 +103,29 @@ export const STORE_CATALOG_SOURCE = [
   },
   {
     "id": "outfit-03",
-    "name": "套裝 03",
+    "name": "魔君再臨",
+    "badge": "PREVIEW",
     "published": true,
     "pieces": {
       "hair": {
         "id": "store-outfit-03-hair",
-        "label": "套裝 03・頭部",
+        "label": "魔君再臨・頭部",
         "front": "store/hair/頭髮03-1.png",
         "back": "store/hair/頭髮03-2.png"
       },
       "face": {
         "id": "store-outfit-03-face",
-        "label": "套裝 03・臉部",
+        "label": "魔君再臨・臉部",
         "front": "store/face/表情03.png"
       },
       "top": {
         "id": "store-outfit-03-top",
-        "label": "套裝 03・上半身",
+        "label": "魔君再臨・上半身",
         "front": "store/top/上衣03.png"
       },
       "bottoms": {
         "id": "store-outfit-03-bottoms",
-        "label": "套裝 03・下半身",
+        "label": "魔君再臨・下半身",
         "front": "store/bottoms/褲裝03.png"
       }
     },
